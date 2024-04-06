e55ad1a536232e0089285cc3964a1f608b672c42
SevenAndrew <82178604+SevenAndrew@users.noreply.github.com>
2024-04-06 20:58:30 +0200

    Initial Commit

---
+++ Notify-iOS17/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Notify-iOS17
+//
+//  Created by Andreas Sauerwein on 06.04.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
